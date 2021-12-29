f40edc21729c8ec259db71e89c1d0152332ee387
Nikola Djordjevic <nikoladjordjevic@nikollace.local>
2021-12-29 11:37:00 +0100

    Init projects

---
+++ trunks/src/main/java/fon/bg/ac/rs/fpis/trunks/TrunksApplication.java
@@ -0,0 +1,13 @@
+package fon.bg.ac.rs.fpis.trunks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrunksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrunksApplication.class, args);
+	}
+
+}
---
+++ trunks/src/test/java/fon/bg/ac/rs/fpis/trunks/TrunksApplicationTests.java
@@ -0,0 +1,13 @@
+package fon.bg.ac.rs.fpis.trunks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrunksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
